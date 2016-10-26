3847d9a3576d4bb865d0110f9f062158164fb0cb
Vangelis Tasoulas <vangelis@tasoulas.net>
2016-10-26 22:22:40 +0200

    Updated the (C) year to 2016

--- infiniband-graphviz.py
+++ infiniband-graphviz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2015 Vangelis Tasoulas <vangelis@tasoulas.net>
+# Copyright (C) 2016 Vangelis Tasoulas <vangelis@tasoulas.net>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
